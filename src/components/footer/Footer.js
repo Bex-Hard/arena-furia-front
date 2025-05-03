5f6b31f6d8e34d7349d16a5f2bf73da04079327f
Rebecca Hardman <rebeccahardman@gmail.com>
2025-05-03 18:57:52 -0300

    criei o componente do footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './footer.css'
+import icon_furia from '../../assets/images/icon_furia.png';
+import instagram from '../../assets/images/external-icons/instagram.svg';
+
+function Footer() {
+    return (
+        <footer className="app-footer">
+            <div className="site-oficial">
+                <a
+                    href="https://www.furia.gg/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="site-oficial"
+                >
+                    visite nosso <i>site oficial</i>
+                </a>
+            </div>
+            <div className="footer-brand">
+                <div className="footer-logo">
+                    <img src={icon_furia} alt="Ícone FURIA" />
+                </div>
+                <div className="footer-title">
+                    <span className="arena">arena </span>
+                    <span className="furia"> furia</span>
+                </div>
+            </div>
+
+            <div className="footer-links">
+                <a
+                    href="https://www.instagram.com/furiagg/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="instagram-link"
+                >
+                    siga FURIA
+                    <img src={instagram} alt="Instagram FURIA" />
+                </a>
+            </div>
+        </footer>
+    )
+}
+export default Footer;
