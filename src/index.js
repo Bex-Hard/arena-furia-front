726624812adc95e947897c3e52d5676a4a1002f6
Rebecca Hardman <rebeccahardman@gmail.com>
2025-05-03 18:58:17 -0300

    algumas alterações no header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './components/index.css';
+import './components/chatroom/chatroom.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
