368d94b14385ee9dc4bfab47742ae8a91ce9c0f7
Rebecca Hardman <rebeccahardman@gmail.com>
2025-05-06 13:52:20 -0300

    alteração das cores do chatbot e melhor diagramação do footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import './footer.css'
-import icon_furia from '../../assets/images/icon_furia.png';
-import instagram from '../../assets/images/external-icons/instagram.svg';
-import telegram from '../../assets/images/external-icons/telegram.svg';
+import './footer.css';
+import iconFuria from '../../assets/images/icon_furia.png';
+import instagramIcon from '../../assets/images/external-icons/instagram.svg';
+import telegramIcon from '../../assets/images/external-icons/telegram.svg';
 
-function Footer() {
+const SOCIAL_LINKS = [
+    {
+        href: 'https://www.instagram.com/furiagg/',
+        label: 'siga FURIA',
+        icon: instagramIcon,
+        alt: 'Instagram FURIA'
+    },
+    {
+        href: 'https://t.me/hard_furioso_bot',
+        label: 'converse com o nosso bot',
+        icon: telegramIcon,
+        alt: 'Telegram FURIA'
+    }
+];
+
+export default function Footer() {
     return (
         <footer className="app-footer">
-            <div className="site-oficial">
+            <div className="footer-left">
                 <a
                     href="https://www.furia.gg/"
                     target="_blank"
@@ -17,37 +32,33 @@ function Footer() {
                     visite nosso <i>site oficial</i>
                 </a>
             </div>
-            <div className="footer-brand">
-                <div className="footer-logo">
-                    <img src={icon_furia} alt="Ícone FURIA" />
-                </div>
-                <div className="footer-title">
-                    <span className="arena">arena</span>
-                    <span className="furia">furia</span>
+
+            <div className="footer-center">
+                <div className="footer-brand">
+                    <img className="footer-logo" src={iconFuria} alt="Ícone FURIA" />
+                    <div className="footer-title">
+                        <span className="arena">arena</span>
+                        <span className="furia">furia</span>
+                    </div>
                 </div>
             </div>
 
-            <div className="footer-links">
-                <a
-                    href="https://www.instagram.com/furiagg/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="instagram-link"
-                >
-                    siga FURIA
-                    <img src={instagram} alt="Instagram FURIA" />
-                </a>
-                <a
-                    href="https://t.me/hard_furioso_bot"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="telegram-link"
-                >
-                    converse com nosso bot
-                    <img src={telegram} alt="Telegram FURIA" />
-                </a>
+            <div className="footer-right">
+                <div className="footer-links">
+                    {SOCIAL_LINKS.map(({ href, label, icon, alt }) => (
+                        <a
+                            key={href}
+                            href={href}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="social-link"
+                        >
+                            {label}
+                            <img src={icon} alt={alt} />
+                        </a>
+                    ))}
+                </div>
             </div>
         </footer>
-    )
+    );
 }
-export default Footer;
